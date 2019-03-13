dfe2542a0c7c88a095573ff781f6556c3bd5fdd6
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-13 12:29:52 -0700

    minor

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,10 @@ const traverse = async (dir: string, action: Action): Promise<void> => {
                 if (!exist) {
                     const fullPath = path.resolve(filePath)
                     console.log(`publishing ${id} from ${fullPath}`)
+                    const cmd = `npm publish ${fullPath} --access=public`
+                    console.log(`running: ${cmd}`)
                     try {
-                        child_process.execSync(`npm publish ${fullPath} --access=public`)
+                        child_process.execSync(cmd)
                         console.log(`${id} done.`)
                     } catch (e) {
                         process.exitCode = 1
