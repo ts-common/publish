21d7cdd7b00de731224900b26ddba274bd1dc56c
Kamil Pajdzik <kamip@microsoft.com>
2019-05-02 13:38:38 -0700

    Add option to respect autoPublish option

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@ import decompress from "decompress"
 interface Package {
     readonly name: string
     readonly version: string
+    readonly autoPublish?: boolean
 }
 
 type Action = "publish"|"unpublish"
 
-const traverse = async (dir: string, action: Action): Promise<void> => {
+const traverse = async (dir: string, action: Action, auto: boolean): Promise<void> => {
     const files = fs.readdirSync(dir, { withFileTypes: true })
     for (const file of files) {
         const filePath = path.join(dir, file.name)
         if (file.isDirectory()) {
-            traverse(filePath, action)
+            traverse(filePath, action, auto)
         } else if (path.extname(file.name) === ".tgz") {
             const files = await decompress(
                 filePath,
@@ -24,8 +25,14 @@ const traverse = async (dir: string, action: Action): Promise<void> => {
                 }
             )
             const packageJson = files[0]
-            const { name, version } = JSON.parse(packageJson.data.toString()) as Package
+            const { name, version, autoPublish } = JSON.parse(packageJson.data.toString()) as Package
             const id = `${name}@${version}`
+
+            if (auto && autoPublish === true) {
+                console.log(`skipping ${id} because `)
+                continue;
+            }
+
             let viewNames: string = ""
             try {
                 viewNames = child_process.execSync(`npm view ${id} name`).toString()
@@ -40,7 +47,8 @@ const traverse = async (dir: string, action: Action): Promise<void> => {
                     const cmd = `npm publish "${fullPath}" --access=public`
                     console.log(`running: ${cmd}`)
                     try {
-                        child_process.execSync(cmd)
+                        console.log(`WHATIF: ${cmd}`)
+                        // child_process.execSync(cmd)
                         console.log(`${id} done.`)
                     } catch (e) {
                         process.exitCode = 1
@@ -53,7 +61,8 @@ const traverse = async (dir: string, action: Action): Promise<void> => {
                 if (exist) {
                     console.log(`unpublishing ${id} from ${filePath}`)
                     try {
-                        child_process.execSync(`npm unpublish ${id}`)
+                        console.log(`WHATIF: npm unpublish ${id}`)
+                        // child_process.execSync(`npm unpublish ${id}`)
                     } catch (e) {
                         process.exitCode = 1
                     }
@@ -68,5 +77,7 @@ const traverse = async (dir: string, action: Action): Promise<void> => {
 // console.log("@ts-common/publish is started")
 // const cwd = path.resolve("./")
 // console.log(`current folder: ${cwd}`)
-traverse("..", process.argv.indexOf("unpublish") >= 0 ? "unpublish" : "publish")
+const action = process.argv.indexOf("unpublish") >= 0 ? "unpublish" : "publish"
+const autoPublish = process.argv.indexOf("auto") >= 0
+traverse("..", action, autoPublish)
 // console.log("@ts-common/publish is done")
